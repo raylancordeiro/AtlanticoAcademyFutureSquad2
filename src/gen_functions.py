9d30f15434c3ac3bb4df9c0c40dae97dc603bf7a
Victor Rocha <victordsrocha@gmail.com>
2021-11-05 23:44:08 -0300

    Update sort_string_list_by_length
    update q3

--- src/gen_functions.py
+++ src/gen_functions.py
@@ -35,21 +35,22 @@ def closest_to_point(points_list, focus_point):
     return closest_point
 
 
-def sort_string_list_by_lenght(string_list, reverse=False):
-    """ Sorts a list of strings by the number of elements in each item
+def sort_string_list_by_length(string_list: list[str], descending: bool = False) -> list[str]:
+    """
+    Sorts a list of strings by the number of elements in each item
 
     Args:
         string_list: list of strings
-        reverse: true for descending order
+        descending: true for descending order and false otherwise
 
     Returns:
         Returns the sorted list
     """
     sorted_list = string_list.copy()
-    if reverse:
-        sorted_list.sort(key=lambda x: len(x))
-    else:
+    if descending:
         sorted_list.sort(key=lambda x: len(x), reverse=True)
+    else:
+        sorted_list.sort(key=lambda x: len(x))
     return sorted_list
 
 
